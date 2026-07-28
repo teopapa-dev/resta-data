61c6e96ba39382a6f6692997362d3a12ca944f19
teopapa-dev <teopapa987@gmail.com>
2026-07-29 00:03:29 +0300

    Update update_products.py

--- update_products.py
+++ update_products.py
@@ -2,7 +2,7 @@ import xml.etree.ElementTree as ET
 import json
 import urllib.request
 
-XML_URL = "https://affiliate.linkwi.se/feeds/1.2/CD28438/programs-joined/columns-product_id,model_name,product_name,description,category,brand_name,tracking_url,thumb_url,image_url,in_stock,availability,valid_from,valid_to,on_sale,currency,price,full_price,discount,city,times_bought,longitude,latitude,address,size,colour,custom,extra_images,variations/catinc-0/catex-0/proginc-0/progex-0/feed.xml"
+XML_URL = "https://affiliate.linkwi.se/feeds/1.2/CD28438/programs-joined/columns-product_id,model_name,product_name,description,category,brand_name,tracking_url,thumb_url,image_url,in_stock,availability,valid_from,valid_to,on_sale,currency,price,full_price,discount,city,times_bought,longitude,latitude,address,size,colour,custom,extra_images,variations/catinc-5,45,69,75,77/catex-0/proginc-14153-2805/progex-0/feed.xml"
 
 def parse_xml_to_json():
     req = urllib.request.Request(XML_URL, headers={'User-Agent': 'Mozilla/5.0'})
